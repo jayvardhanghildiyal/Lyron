44d2abec9e7f2d520864d6ce0f152675e8cd9b3d
jayvardhanghildiyal <jayvghildiyal2016@gmail.com>
2023-06-21 22:05:58 +0530

    added a reload command to, well, reload the slash commands without needing to restart the bot!

---
+++ commands/utility/reload.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command.')
+		.addStringOption(option =>
+			option.setName('command')
+				.setDescription('The command to reload.')
+				.setRequired(true)),
+
+    //try to find the command that we need to reload
+	async execute(interaction) {
+		const commandName = interaction.options.getString('command', true).toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+		}
+         
+        //require() caches the file so if we were to require it again we would load the previously cached file without any changes and that is why we first need to delete the file from require.cache and only then should we require and set the command file to client.commands collection
+		delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
+
+		try {
+	        interaction.client.commands.delete(command.data.name);
+	        const newCommand = require(`../${command.category}/${command.data.name}.js`);
+	        interaction.client.commands.set(newCommand.data.name, newCommand);
+	        await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+		} catch (error) {
+	        console.error(error);
+	        await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+		}
+	},
+};
